2ad49624a340b76bbc6e3bf81c0f36335a6430fa
fw20-0769 <tarakkarmakar1434@gmail.com>
2023-02-15 19:24:07 +0530

    explore page

--- eco-store/src/Pages/explore_page/Explore.jsx
+++ eco-store/src/Pages/explore_page/Explore.jsx
@@ -1,8 +1,57 @@
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useSearchParams } from "react-router-dom";
+import Filter from "../../components/filter/Filter"
+import ProductCard from "../../components/ProductCard/ProductCard";
+import { getProducts, getProductsKitchen } from "../../Redux/Products/action";
 
-const Explore = () => {
+import exp from "./explore.module.css"
+
+const Explore= () => {
+    const dispatch = useDispatch();
+    const { isLoading ,prod} = useSelector((state) => {
+        return {
+          isLoading: state.Allproducts.isLoading,
+          prod :state.Allproducts.products
+        };
+      });
+   
+
+    const location = useLocation();
+    const [searchParams] = useSearchParams();
+  
+    useEffect(() => {
+      if (location || prod.length == 0) {
+        const dataparams = {
+          params: {
+            sort: "price",
+            order: searchParams.getAll("order"),
+            brand: searchParams.get("brand"),
+            off:searchParams.get("discount")
+          },
+        };
+  console.log(dataparams)
+        dispatch(getProducts(dataparams));
+      }
+    }, [searchParams, dispatch, location]);
   return (
-    <div>
+    <div className={exp.main_container}>
+
+
+        <div className={exp.left_filter_box}><Filter one="" two="sanhya dale" three="wood nature" /></div>
+
+        <div className={exp.right_product_box}>
+        {prod.map((ele) => {
+            
+                return <ProductCard ele={ele} key={ele._id} />;
+              
+            
+
+        }
+        )}
+            </div>
+   
       
     </div>
   )
