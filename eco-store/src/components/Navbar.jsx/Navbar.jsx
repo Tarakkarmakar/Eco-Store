d3e5be60a20a19e24d92cd021e402d2552eb5b53
fw20-0769 <tarakkarmakar1434@gmail.com>
2023-02-21 02:00:24 +0530

    login problem is fixed

--- eco-store/src/components/Navbar.jsx/Navbar.jsx
+++ eco-store/src/components/Navbar.jsx/Navbar.jsx
@@ -1,5 +1,14 @@
 import css from "./Navbar.module.css";
-import { Input, InputGroup, InputRightElement, Button, Stack, Avatar, AvatarBadge } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Stack,
+  Avatar,
+  AvatarBadge,
+  useToast,
+} from "@chakra-ui/react";
 import logo from "../../images/logo.png";
 import bag from "../../images/bag.png";
 import { HamburgerIcon, Search2Icon } from "@chakra-ui/icons";
@@ -12,105 +21,119 @@ import { SIGNIN_FAILURE } from "../../Redux/SignUpReducer/actionTypes";
 const Navbar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [aboutUser,setAboutUser]=useState(false)
-
+  const [aboutUser, setAboutUser] = useState(false);
+  const toast = useToast();
   const goSignup = () => {
     navigate("/login");
   };
-  
-  const gotoHome=()=>{
-
-    navigate("/")
-  }
-  const gotoBag=()=>{
-
-    navigate("/bag")
-  }
-  const gotoPartner=()=>{
-
-    navigate("/partner")
-  }
-let [user,setUser]=useState(JSON.parse(localStorage.getItem("email")) || "")
-
-const { isAuth, isError, isLoading } = useSelector((state) => {
-  return {
-    isAuth: state.signUpReducer.isAuth,
-    isError: state.signUpReducer.isError,
-    isLoading: state.signUpReducer.isLoading,
+  const [logedUser, setloggedUser] = useState(true);
+  const gotoHome = () => {
+    navigate("/");
   };
-});
+  const gotoBag = () => {
+    navigate("/bag");
+  };
+  const gotoPartner = () => {
+    navigate("/partner");
+  };
+  let [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("email")) || ""
+  );
 
-useEffect(()=>{
+  const { isAuth, isError, isLoading } = useSelector((state) => {
+    return {
+      isAuth: state.signUpReducer.isAuth,
+      isError: state.signUpReducer.isError,
+      isLoading: state.signUpReducer.isLoading,
+    };
+  });
 
-  setUser(JSON.parse(localStorage.getItem("email")))
-if(user!==""){
-  dispatch(AutoSignIn(user))
-}
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("email")));
+  }, []);
+  const openDiv = () => {
+    setUser(JSON.parse(localStorage.getItem("email")));
+    setAboutUser(!aboutUser);
+  };
 
-},[])
-const openDiv=()=>{
-setAboutUser(!aboutUser)
+  const logOut = () => {
+    localStorage.setItem("email", null);
+    setUser(JSON.parse(localStorage.getItem("email")));
 
-}
+    setAboutUser(!aboutUser);
 
+    dispatch(AutoSignIn("empty"));
 
+    setloggedUser(false);
+    toast({
+      title: `Log out successfull`,
+      status: "success",
+      duration: 1500,
+      position: "top",
+      isClosable: true,
+    });
 
-console.log(isAuth)
+    window.location.reload();
+  };
+
   return (
     <>
-    <div className={css.main_nav}>
-      <div className={css.nav_left_section} onClick={gotoHome}>
-        <img className={css.nav_logo} src={logo} alt="" />
+      <div className={css.main_nav}>
+        <div className={css.nav_left_section} onClick={gotoHome}>
+          <img className={css.nav_logo} src={logo} alt="" />
+        </div>
+        <div>
+          <h3 className={css.nav_company_name}>Eco Store</h3>
+        </div>
+
+        <div className={css.nav_serach_section}>
+          <InputGroup size="md">
+            <Input pr="4.5rem" focusBorderColor="lime" background="white" />
+            <InputRightElement width="2.5rem">
+              <Button h="1.75rem" size="sm">
+                <Search2Icon />
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </div>
+
+        <div className={css.nav_right_section}>
+          <ul className={css.nav_right_list}>
+            {isAuth ? (
+              <li onClick={openDiv}>
+                <Avatar>
+                  <AvatarBadge boxSize="1.25em" bg="blue.500" />
+                </Avatar>
+              </li>
+            ) : (
+              <li onClick={goSignup}>
+                {" "}
+                <img className={css.nav_account} src={account} />{" "}
+                <p> SignUp/Login</p>
+              </li>
+            )}
+
+            <li onClick={gotoPartner}>Become a Partner</li>
+            <li onClick={gotoBag}>
+              <img className={css.nav_bag} src={bag} alt="" />
+              <p>Bag</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className={css.nav_hamberger}>
+          <HamburgerIcon h="2.3rem" w="2.2rem" />
+        </div>
       </div>
-      <div>
-        <h3 className={css.nav_company_name}>Eco Store</h3>
-      </div>
+      {aboutUser && logedUser ? (
+        <div className={css.opendiv}>
+          <h2>{user}</h2>
 
-      <div className={css.nav_serach_section}>
-        <InputGroup size="md">
-          <Input pr="4.5rem" focusBorderColor="lime" background="white" />
-          <InputRightElement width="2.5rem">
-            <Button h="1.75rem" size="sm">
-              <Search2Icon />
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </div>
-
-      <div className={css.nav_right_section}>
-        <ul className={css.nav_right_list}>
-{isAuth? 
-<li onClick={openDiv}>
-  <Avatar>
-    <AvatarBadge boxSize='1.25em' bg='blue.500' />
-  </Avatar>
-  </li>
-  :  <li onClick={goSignup}>
-            {" "}
-            <img className={css.nav_account} src={account} />{" "}
-            <p> SignUp/Login</p>
-          </li> }
-      
-        
-
-          <li onClick={gotoPartner}>Become a Partner</li>
-          <li onClick={gotoBag}>
-            <img className={css.nav_bag} src={bag} alt="" />
-            <p>Bag</p>
-          </li>
-        </ul>
-      </div>
-
-      <div className={css.nav_hamberger}>
-        <HamburgerIcon h="2.3rem" w="2.2rem" />
-      </div>
-    </div>
-  {aboutUser ? <div className={css.opendiv}>
-   <h2>{user}</h2>
-
-   <button >log out</button>
-
-  </div> :""}
+          <button onClick={logOut}>log out</button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
