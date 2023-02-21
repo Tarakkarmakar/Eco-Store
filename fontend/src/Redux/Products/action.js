3985e8c2e56d592dcbbd0a30c20cb08a205ada46
fw20-0769 <tarakkarmakar1434@gmail.com>
2023-02-21 19:51:23 +0530

    home page product responsive

---
+++ fontend/src/Redux/Products/action.js
@@ -0,0 +1,103 @@
+import * as types from "./actionTypes";
+
+import axios from "axios";
+
+const getProductsRequest = () => {
+  return {
+    type: types.GET_PRODUCTS_REQUEST,
+  };
+};
+
+const getProductsSuccess = (payload) => {
+  return {
+    type: types.GET_PRODUCTS_SUCCESS,
+    payload,
+  };
+};
+
+const getProductssError = () => {
+  return {
+    type: types.GET_PRODUCTS_FAILURE,
+  };
+};
+const getProducts = (params) => (dispatch) => {
+
+
+  dispatch(getProductsRequest());
+  return axios
+    .get(`${process.env.REACT_APP_API}/customerproducts`, params)
+    .then((r) => {
+      console.log(r.data);
+      dispatch(getProductsSuccess(r.data));
+    })
+    .catch((e) => {
+      dispatch(getProductssError());
+    });
+};
+
+const getProductsKitchen = (params) => (dispatch) => {
+  dispatch(getProductsRequest());
+  return axios
+    .get(`${process.env.REACT_APP_API}/customerproducts/kitchen`, params)
+    .then((r) => {
+      console.log(r.data);
+      dispatch(getProductsSuccess(r.data));
+    })
+    .catch((e) => {
+      dispatch(getProductssError());
+    });
+};
+const getProductsFashion = (params) => (dispatch) => {
+  dispatch(getProductsRequest());
+  return axios
+    .get(`${process.env.REACT_APP_API}/customerproducts/fashion`, params)
+    .then((r) => {
+      console.log(r.data);
+      dispatch(getProductsSuccess(r.data));
+    })
+    .catch((e) => {
+      dispatch(getProductssError());
+    });
+};
+const getProductsPlants = (params) => (dispatch) => {
+  dispatch(getProductsRequest());
+  return axios
+    .get(`${process.env.REACT_APP_API}/customerproducts/plants`, params)
+    .then((r) => {
+      console.log(r.data);
+      dispatch(getProductsSuccess(r.data));
+    })
+    .catch((e) => {
+      dispatch(getProductssError());
+    });
+};
+
+
+const getSingleData = (id) =>(dispatch)=> {
+  dispatch(getProductsRequest());
+  axios
+    .get(`${process.env.REACT_APP_API}/customerproducts/single/${id}`)
+    .then((r) => {
+      dispatch(getProductsSuccess(r.data));
+    }).catch((e) => {
+      dispatch(getProductssError());
+      console.log("error product");
+    });
+};
+
+const getUserBagProduct=(email)=>(dispatch)=>{
+
+  dispatch(getProductsRequest());
+  axios
+    .get(`${process.env.REACT_APP_API}/cart`,{
+      headers:{"Authorization":email}
+    })
+    .then((r) => {
+      dispatch(getProductsSuccess(r.data));
+    }).catch((e) => {
+      dispatch(getProductssError());
+      console.log("error product");
+    });
+}
+
+export { getProducts,getSingleData,getUserBagProduct , getProductsKitchen,getProductsFashion,getProductsPlants };
---
+++ fontend/src/components/Navbar.jsx/Navbar.jsx
@@ -0,0 +1,141 @@
+import css from "./Navbar.module.css";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Stack,
+  Avatar,
+  AvatarBadge,
+  useToast,
+} from "@chakra-ui/react";
+import logo from "../../images/logo.png";
+import bag from "../../images/bag.png";
+import { HamburgerIcon, Search2Icon } from "@chakra-ui/icons";
+import account from "../../images/account.jpg";
+import { json, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { AutoSignIn } from "../../Redux/SignUpReducer/action";
+import { SIGNIN_FAILURE } from "../../Redux/SignUpReducer/actionTypes";
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [aboutUser, setAboutUser] = useState(false);
+  const toast = useToast();
+  const goSignup = () => {
+    navigate("/login");
+  };
+  const [logedUser, setloggedUser] = useState(true);
+  const gotoHome = () => {
+    navigate("/");
+  };
+  const gotoBag = () => {
+    navigate("/bag");
+  };
+  const gotoPartner = () => {
+    navigate("/partner");
+  };
+  let [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("email")) || ""
+  );
+
+  const { isAuth, isError, isLoading } = useSelector((state) => {
+    return {
+      isAuth: state.signUpReducer.isAuth,
+      isError: state.signUpReducer.isError,
+      isLoading: state.signUpReducer.isLoading,
+    };
+  });
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("email")));
+  }, []);
+  const openDiv = () => {
+    setUser(JSON.parse(localStorage.getItem("email")));
+    setAboutUser(!aboutUser);
+  };
+
+  const logOut = () => {
+    localStorage.setItem("email", null);
+    setUser(JSON.parse(localStorage.getItem("email")));
+
+    setAboutUser(!aboutUser);
+
+    dispatch(AutoSignIn("empty"));
+
+    setloggedUser(false);
+    toast({
+      title: `Log out successfull`,
+      status: "success",
+      duration: 1500,
+      position: "top",
+      isClosable: true,
+    });
+
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <div className={css.main_nav}>
+        <div className={css.nav_left_section} onClick={gotoHome}>
+          <img className={css.nav_logo} src={logo} alt="" />
+        </div>
+        <div>
+          <h3 className={css.nav_company_name}>Eco Store</h3>
+        </div>
+
+        <div className={css.nav_serach_section}>
+          <InputGroup size="md">
+            <Input pr="4.5rem" focusBorderColor="lime" background="white" />
+            <InputRightElement width="2.5rem">
+              <Button h="1.75rem" size="sm">
+                <Search2Icon />
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </div>
+
+        <div className={css.nav_right_section}>
+          <ul className={css.nav_right_list}>
+            {isAuth ? (
+              <li onClick={openDiv}>
+                <Avatar>
+                  <AvatarBadge boxSize="1.25em" bg="blue.500" />
+                </Avatar>
+              </li>
+            ) : (
+              <li onClick={goSignup}>
+                {" "}
+                <img className={css.nav_account} src={account} />{" "}
+                <p> SignUp/Login</p>
+              </li>
+            )}
+
+            <li onClick={gotoPartner}>Become a Partner</li>
+            <li onClick={gotoBag}>
+              <img className={css.nav_bag} src={bag} alt="" />
+              <p>Bag</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className={css.nav_hamberger}>
+          <HamburgerIcon h="2.3rem" w="2.2rem" />
+        </div>
+      </div>
+      {aboutUser && logedUser ? (
+        <div className={css.opendiv}>
+          <h2>{user}</h2>
+
+          <button onClick={logOut}>log out</button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default Navbar;
