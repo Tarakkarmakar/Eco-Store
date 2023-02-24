7b73effb03a6e98742b66eb0a340eda9ce2421b3
fw20-0769 <tarakkarmakar1434@gmail.com>
2023-02-24 23:28:05 +0530

    order products

--- fontend/src/Pages/Bag/Bag.jsx
+++ fontend/src/Pages/Bag/Bag.jsx
@@ -35,10 +35,9 @@ import {
   PinInputField,
   useDisclosure,
   useToast,
- 
 } from "@chakra-ui/react";
 import axios from "axios";
-import validator from 'validator' 
+import validator from "validator";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
@@ -55,40 +54,37 @@ const Bag = () => {
   const [userToken, setUserToken] = useState(localStorage.getItem("email"));
   const dispatch = useDispatch();
   const navigate = useNavigate();
- 
+
   const [placedOrder, setPlacedOrder] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
   const [deliveryPrice, setDeliveryPrice] = useState(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const toast=useToast()
+  const toast = useToast();
   const btnRef = useRef();
 
-  const [otp, setOtp] = useState('');
-  const [userOtp,setUserOtp]=useState('')
-  const [payment,setPayment]=useState(false)
-  const [openModal,setOpenModal] = useState();
-
-  const [otpBox,setOtpBox]=useState(false)
-  const cancelRef =useRef()
+  const [otp, setOtp] = useState("");
+  const [userOtp, setUserOtp] = useState("");
+  const [payment, setPayment] = useState(false);
+  const [openModal, setOpenModal] = useState();
+  const [loader, setLoader] = useState(false);
+  const [otpBox, setOtpBox] = useState(false);
+  const cancelRef = useRef();
   ////Address state///
-const [locality,setLocality]=useState("")
+  const [locality, setLocality] = useState("");
 
-const [district,setDistrict]=useState("")
+  const [district, setDistrict] = useState("");
 
-const [state,setState]=useState("")
+  const [state, setState] = useState("");
 
-const [landmark,setLandmark]=useState("")
+  const [landmark, setLandmark] = useState("");
 
-const [pincode,setPincode]=useState("")
+  const [pincode, setPincode] = useState("");
 
-const [mobile,setMobile]=useState("")
-//---------//
+  const [mobile, setMobile] = useState("");
+  //---------//
 
- 
+  const Addres = `Locality `;
 
-const Addres=`Locality `
-
-  
   const { isLoading, product } = useSelector((state) => {
     return {
       isLoading: state.Allproducts.isLoading,
@@ -102,257 +98,366 @@ const Addres=`Locality `
       isLoading: state.signUpReducer.isLoading,
     };
   });
-//get specific user cart products//
+  //get specific user cart products//
   useEffect(() => {
     setUserToken(localStorage.getItem("email"));
     dispatch(getUserBagProduct(userToken));
+
+
+    let total_temp = 0;
+
+
+    if (product.length > 0) {
+      total_temp = product.reduce((total, item) => total + item.price, 0);
+  
+      if (total_temp < 400) {
+        setDeliveryPrice(40);
+        total_temp += 40;
+      }
+    }
+
+    setTotalPrice(total_temp)
   }, [isAuth]);
 
-////total product calculation//
-  let total_temp = 0;
-  if (product.length > 0) {
-    total_temp = product.reduce((total, item) => total + item.price, 0);
+  ////total product calculation//
+  
 
-    if (total_temp < 400) {
-      setDeliveryPrice(40);
-      total_temp += 40;
+
+
+  
+
+
+
+  //order post to database(API)
+
+  const handleProced = () => {
+    if (
+      locality === "" ||
+      district == "" ||
+      state === "" ||
+      landmark === "" ||
+      pincode == "" ||
+      mobile == ""
+    ) {
+      toast({
+        title: `Please Enter all the feilds!`,
+        status: "error",
+        duration: 2000,
+        position: "top",
+        isClosable: true,
+      });
+    } else if (pincode < 100000 || pincode > 999999) {
+      toast({
+        title: `Please Enter a valid pincode(india) !`,
+        status: "error",
+        duration: 2000,
+        position: "top",
+        isClosable: true,
+      });
+    } else if (mobile < 1000000000 || mobile > 9999999999) {
+      toast({
+        title: `Please Enter a valid Mobile number !`,
+        status: "error",
+        duration: 2000,
+        position: "top",
+        isClosable: true,
+      });
+    } else {
+      setOpenModal(true);
     }
-  }
 
-
-
-
- //order post to database(API)
-
-const handleProced=()=>{
-
-
-  if(locality==="" || district=="" || state==="" || landmark==="" || pincode=="" || mobile==""){
-
-    toast({
-      title: `Please Enter all the feilds!`,
-      status: "error",
-      duration: 2000,
-      position: "top",
-      isClosable: true,
-    });
-  
-  }else if(pincode<100000 || pincode>999999){
-    toast({
-      title: `Please Enter a valid pincode(india) !`,
-      status: "error",
-      duration: 2000,
-      position: "top",
-      isClosable: true,
-    });
-  
-  }else if(mobile<1000000000 || mobile>9999999999){
-    toast({
-      title: `Please Enter a valid Mobile number !`,
-      status: "error",
-      duration: 2000,
-      position: "top",
-      isClosable: true,
-    });
-  }
-  else{
-   
-    setOpenModal(true)
-
-
-  }
-  
-  console.log(locality,district,state,landmark,pincode,mobile)
-
-}
-///OTP genration--//
-const  handleGenerateOTP=()=>{
-  setOtp(generateOTP())
-if(otp){
-  toast({
-    title: `Your OTP is ${otp}`,
-    // description: "",
-    position:'top',
-    status: 'success',
-    duration: 10000,
-    isClosable: true,
-    
-  })
-
-setOpenModal(!openModal)
-  setOtpBox(true)
-}
- 
-
-  
-  
-}
-console.log("user -OTp",userOtp)
-const cancelPayment=()=>{
-setOtpBox(false)
-setOtp('')
-setUserOtp('')
-
-}
-  const procedPayment = () => {
-    axios.post(`${process.env.REACT_APP_API}/orders/create`);
+    console.log(locality, district, state, landmark, pincode, mobile);
   };
- 
+  ///OTP genration--//
+  const handleGenerateOTP = () => {
+    setOtp(generateOTP());
+    if (otp) {
+      toast({
+        title: `Your OTP is ${otp}`,
+        // description: "",
+        position: "top",
+        status: "success",
+        duration: 10000,
+        isClosable: true,
+      });
+
+      setOpenModal(!openModal);
+      setOtpBox(true);
+    }
+  };
+  console.log("user -OTp", userOtp);
+
+  const cancelPayment = () => {
+    setOtpBox(false);
+
+    setOtp("");
+
+    setUserOtp("");
+  };
+  const procedPayment = () => {
+    setLoader(true);
+
+    if (otp == userOtp) {
+      setPayment(true);
+
+      toast({
+        title: `Payment SuccessFull `,
+        // description: "",
+        position: "top",
+        status: "success",
+        duration: 1000,
+        isClosable: true,
+      });
+    }
+    const email = JSON.parse(localStorage.getItem("email"));
+    const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+    const userID = userDetails._id;
+    const addres = `Locality:${locality}District:${district}
+State:${state}Landmark:${landmark}
+pincode:${pincode}Mobile:${mobile}`;
+    let count = 0;
+
+    product.map((ele) => {
+      const order = {
+        ProductId: ele._id,
+        title: ele.title,
+        image: ele.image,
+        brand: ele.brand,
+        category: ele.category,
+        Rating: ele.Rating,
+        payment: payment,
+        price: ele.price,
+        email: email,
+        orderedBy: userID,
+        Mobile: mobile,
+        count: ele.count,
+        address: addres,
+        deliverStatus: false,
+      };
+
+      console.log(order);
+
+      // axios.post(`${process.env.REACT_APP_API}/orders/create`,order)
+      //   .then((r)=>{
+      //   count++
+      // console.log(r)
+      //    }).catch((e)=>{
+      //     toast({
+      //       title: `Failed to order `,
+      //      // description: "",
+      //     position:'top',
+      //       status: 'success',
+      //        duration: 1000,
+      //        isClosable: true,
+
+      //      })
+      //      console.log(e)
+      //   })
+    });
+
+    // if(count==product.length){
+    //   toast({
+    //     title: `Order placed succesfull `,
+    //     // description: "",
+    //     position:'top',
+    //     status: 'success',
+    //     duration: 1000,
+    //     isClosable: true,
+
+    //   })
+    // }
+  };
+
   return (
     <>
-    <div className={bag.main_div}>
-      <div className={bag.left_product_list}>
-        {product.length > 0 ? (
-          product.map((ele) => {
-            return <ProductCard ele={ele} key={ele._id} />;
-          })
-        ) : (
-          <h1>You have not added any product !</h1>
-        )}
+      <div className={bag.main_div}>
+        <div className={bag.left_product_list}>
+          {product.length > 0 ? (
+            product.map((ele) => {
+              return <ProductCard ele={ele} key={ele._id} />;
+            })
+          ) : (
+            <h1>You have not added any product !</h1>
+          )}
+        </div>
+        <div className={bag.right_total_product}>
+          <div className={bag.first_div}>
+            <ul>
+              {product.length > 0 &&
+                product.map((ele) => {
+                  return (
+                    <li>
+                      <h3>{ele.brand}....</h3> <h2>Rs.{ele.price}</h2>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+          <div className={bag.sec_div}>
+            <ul>
+              <li>
+                <h3>GST % </h3> <h2 className={bag.gst_price}>RS.0</h2>{" "}
+              </li>
+              <li>
+                <h3 className={bag.delivery_price}>
+                  Delivery fee &nbsp; &nbsp;
+                </h3>{" "}
+                <h3>Rs.{deliveryPrice}</h3>
+              </li>
+            </ul>
+          </div>
+          <div className={bag.third_div}>
+            <ul>
+              <li>
+                <h3>Total &nbsp;</h3> <h2>RS.{totalPrice}</h2>
+              </li>
+              <br />
+              <li>
+                <h3>Deliery Expected :- 7-10 Days</h3>
+              </li>
+            </ul>
+          </div>
+
+          <div className={bag.last_div}>
+            <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+              Place order
+            </Button>
+
+            <Drawer
+              isOpen={isOpen}
+              placement="right"
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Enter Your address</DrawerHeader>
+
+                <DrawerBody>
+                  <Input
+                    placeholder="locality/Block"
+                    value={locality}
+                    onChange={(e) => setLocality(e.target.value)}
+                    marginBottom="1.2rem"
+                  />
+
+                  <Input
+                    placeholder="District/city"
+                    value={district}
+                    onChange={(e) => setDistrict(e.target.value)}
+                    marginBottom="1.2rem"
+                  />
+                  <Input
+                    placeholder="State"
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    marginBottom="1.2rem"
+                  />
+                  <Input
+                    placeholder="Land Mark"
+                    value={landmark}
+                    onChange={(e) => setLandmark(e.target.value)}
+                    marginBottom="1.2rem"
+                  />
+                  <Input
+                    placeholder="Pincode"
+                    marginBottom="1.2rem"
+                    onChange={(e) => setPincode(e.target.value)}
+                    value={pincode}
+                  />
+
+                  <Input
+                    placeholder="Mobile number"
+                    value={mobile}
+                    onChange={(e) => setMobile(e.target.value)}
+                  />
+                </DrawerBody>
+
+                <DrawerFooter>
+                  <Button variant="outline" mr={3} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="blue" onClick={handleProced}>
+                    Procced
+                  </Button>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+          </div>
+
+          <h3> @ free Delivery on order over 400</h3>
+        </div>
       </div>
-      <div className={bag.right_total_product}>
-        <div className={bag.first_div}>
-          <ul>
-            {product.length > 0 &&
-              product.map((ele) => {
-                return (
-                  <li>
-                    <h3>{ele.brand}....</h3> <h2>Rs.{ele.price}</h2>
-                  </li>
-                );
-              })}
-          </ul>
-        </div>
-        <div className={bag.sec_div}>
-          <ul>
-            <li>
-              <h3>GST % </h3> <h2 className={bag.gst_price}>RS.0</h2>{" "}
-            </li>
-            <li>
-              <h3 className={bag.delivery_price}>Delivery fee &nbsp; &nbsp;</h3>{" "}
-              <h3>Rs.{deliveryPrice}</h3>
-            </li>
-          </ul>
-        </div>
-        <div className={bag.third_div}>
-          <ul>
-            <li>
-              <h3>Total &nbsp;</h3> <h2>RS.{total_temp}</h2>
-            </li>
-            <br />
-            <li><h3>Deliery Expected :- 7-10 Days</h3></li>
-          </ul>
-        </div>
+      <Modal isOpen={openModal} onClose={() => setOpenModal(!openModal)}>
+        <ModalOverlay />
+        <ModalContent>
+          <h1 className={bag.modalheader}>Choose Payment</h1>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormLabel>Toal;- INR. {totalPrice}</FormLabel>
+            <Box>
+              <FormLabel>Pay Via UPI</FormLabel>
+              <InputGroup size="md">
+                <Input pr="4.5rem" placeholder="Eg;- 8945553710@upi" />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleGenerateOTP}>
+                    Verify
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Checkbox colorScheme="green" marginTop="1.5rem">
+                Cash on Delivery
+              </Checkbox>
+            </Box>
+          </ModalBody>
 
-        <div className={bag.last_div}>
+          <ModalFooter>
+            <Button
+              colorScheme="red"
+              mr={3}
+              onClick={() => setOpenModal(!openModal)}
+            >
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Procced Next</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
 
-
-          <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-            Place order
-          </Button>
-
-          <Drawer
-            isOpen={isOpen}
-            placement="right"
-            onClose={onClose}
-            finalFocusRef={btnRef}
-          >
-            <DrawerOverlay />
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerHeader>Enter Your address</DrawerHeader>
-
-              <DrawerBody>
-                <Input placeholder="locality/Block" value={locality} onChange={(e)=>setLocality(e.target.value)}marginBottom="1.2rem" />
-
-                <Input placeholder="District/city" value={district} onChange={(e)=>setDistrict(e.target.value)} marginBottom="1.2rem" />
-                <Input placeholder="State" value={state} onChange={(e)=>setState(e.target.value)} marginBottom="1.2rem" />
-                <Input placeholder="Land Mark" value={landmark} onChange={(e)=>setLandmark(e.target.value)} marginBottom="1.2rem" />
-                <Input placeholder="Pincode" marginBottom="1.2rem" onChange={(e)=>setPincode(e.target.value)} value={pincode} />
-                  
-                  <Input placeholder="Mobile number" value={mobile} onChange={(e)=>setMobile(e.target.value)} />
-               
-              </DrawerBody>
-
-              <DrawerFooter>
-                <Button variant="outline" mr={3} onClick={onClose}>
-                  Cancel
-                </Button>
-                <Button colorScheme="blue" onClick={handleProced}>Procced</Button>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-        </div>
-
-        <h3> @ free Delivery on order over 400</h3>
-      </div>
-    </div>
-    <Modal isOpen={openModal} onClose={()=>setOpenModal(!openModal)}>
-  <ModalOverlay/>
-  <ModalContent>
-  <h1 className={bag.modalheader}>Choose Payment</h1>
-    <ModalCloseButton />
-    <ModalBody>
-  <FormLabel>Toal;-    INR.  {total_temp}</FormLabel>
-       <Box >
-        <FormLabel>Pay Via UPI</FormLabel>
-       <InputGroup size='md'>
-      <Input
-        pr='4.5rem'
-      
-        placeholder='Eg;- 8945553710@upi'
-      />
-      <InputRightElement width='4.5rem'>
-        <Button h='1.75rem'  size='sm' onClick={handleGenerateOTP} >
-        Verify 
-        </Button>
-      </InputRightElement>
-    </InputGroup>
-    <Checkbox colorScheme='green' marginTop="1.5rem" >
- Cash on Delivery
-  </Checkbox>
-
-       </Box>
- 
-    </ModalBody>
-
-    <ModalFooter>
-      <Button colorScheme='red' mr={3} onClick={()=>setOpenModal(!openModal)}>
-        Cancel
-      </Button>
-      <Button colorScheme="blue">Procced Next</Button>
-    </ModalFooter>
-  </ModalContent>
-</Modal>
-
-
-<AlertDialog
+      <AlertDialog
         isOpen={otpBox}
         leastDestructiveRef={cancelRef}
-        onClose={()=>setOtpBox(!otpBox)}
+        onClose={() => setOtpBox(!otpBox)}
       >
         <AlertDialogOverlay>
           <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-             Enter OTP
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Enter OTP
             </AlertDialogHeader>
 
             <AlertDialogBody marginBottom="1.5rem">
               You are 100% Secure with us
             </AlertDialogBody>
             <HStack marginLeft="27%">
-  <PinInput otp mask size='lg' value={userOtp}  onChange={(value)=>setUserOtp(value)}>
-    <PinInputField  />
-    <PinInputField />
-    <PinInputField  />
-    <PinInputField  />
-  </PinInput>
-</HStack>
+              <PinInput
+                otp
+                mask
+                size="lg"
+                value={userOtp}
+                onChange={(value) => setUserOtp(value)}
+              >
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+              </PinInput>
+            </HStack>
             <AlertDialogFooter>
               <Button ref={cancelRef} onClick={cancelPayment}>
                 Cancel
               </Button>
-              <Button colorScheme='red'  ml={3}>
+              <Button colorScheme="red" ml={3} onClick={procedPayment}>
                 complete Payment
               </Button>
             </AlertDialogFooter>
